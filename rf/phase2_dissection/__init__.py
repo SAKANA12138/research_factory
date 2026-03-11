888eea5875def33eaf79bf1eac3add11c60c22dc
Ruiqi Chen <139046165+SAKANA12138@users.noreply.github.com>
2026-03-11 15:52:41 +0800

    Update __init__.py

--- rf/phase2_dissection/__init__.py
+++ rf/phase2_dissection/__init__.py
@@ -1,1 +1,5 @@
+from rf.phase2_dissection.source_archaeologist import SourceArchaeologist
+from rf.phase2_dissection.math_translator import MathTranslator
+from rf.phase2_dissection.conflict_detector import ConflictDetector
 
+__all__ = ["SourceArchaeologist", "MathTranslator", "ConflictDetector"]
