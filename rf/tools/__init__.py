f202bc487b9b373e61a37431ba94f166ab979126
Ruiqi Chen <139046165+SAKANA12138@users.noreply.github.com>
2026-03-11 15:49:16 +0800

    Update __init__.py

--- rf/tools/__init__.py
+++ rf/tools/__init__.py
@@ -1,1 +1,5 @@
+from rf.tools.arxiv_crawler import search_arxiv
+from rf.tools.github_stars import fetch_trending_repos
+from rf.tools.openreview_client import search_openreview
 
+__all__ = ["search_arxiv", "fetch_trending_repos", "search_openreview"]
